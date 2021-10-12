7b817baf26d30410bc8f43c553eeeb0c6425d8dc
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-10-12 22:07:09 +0200

    Added command get_cards

--- help.py
+++ help.py
@@ -1,3 +1,3 @@
 def help_command(update, context):
-	help_message = "I comandi disponibili sono:\n/start\n/help\n/html"
+	help_message = "I comandi disponibili sono:\n/start\n/help\n/get_cards"
 	context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)
