eb64737e7d795a39086ca3f013821a1455bbb746
AbhijithNT <abhijithnt64@gmail.com>
2021-10-13 19:41:28 +0530

    get_cards command

--- main.py
+++ main.py
@@ -4,34 +4,37 @@ from telegram.ext import MessageHandler, Filters
 
 import logging
 
-import help # Command
-import start # Command
-import cards_list # Command
-import search # Command
+import help  # Command
+import start  # Command
+import cards_list  # Command
+import search  # Command
+
 
 def main() -> None:
-	# Reading token
-	with open("token", "r") as token_file:
-		mytoken = token_file.read()
+    # Reading token
+    with open("token", "r") as token_file:
+        my_token = token_file.read()
 
-	updater = Updater(token=mytoken, use_context=True)
-	dispatcher = updater.dispatcher
-	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    updater = Updater(token=my_token, use_context=True)
+    dispatcher = updater.dispatcher
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-	start_handler = CommandHandler('start', start.start_command)
-	dispatcher.add_handler(start_handler)
+    start_handler = CommandHandler('start', start.start_command)
+    dispatcher.add_handler(start_handler)
 
-	help_handler = CommandHandler('help', help.help_command)
-	dispatcher.add_handler(help_handler)
+    help_handler = CommandHandler('help', help.help_command)
+    dispatcher.add_handler(help_handler)
 
-	card_list_handler = CommandHandler('list_most_wanted_cards', cards_list.get_cards_list)
-	dispatcher.add_handler(card_list_handler)
+    card_list_handler = CommandHandler(
+        ['get_cards', 'list_most_wanted_cards'], cards_list.get_cards_list)
+    dispatcher.add_handler(card_list_handler)
 
-	search_handler = CommandHandler('search', search.search_card)
-	dispatcher.add_handler(search_handler)
+    search_handler = CommandHandler('search', search.search_card)
+    dispatcher.add_handler(search_handler)
+    updater.start_polling()
+    updater.idle()
 
-	updater.start_polling()
-	updater.idle()
 
 if __name__ == "__main__":
-	main()
+    main()
