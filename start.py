019fe98065ecaaab00ec15a120e605daf1f0fdf2
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-10-13 13:27:18 +0200

    Removed version number and changed welcome message

--- start.py
+++ start.py
@@ -1,2 +1,2 @@
 def start_command(update, context):
-	context.bot.send_message(chat_id=update.effective_chat.id, text="Ciao, benvenuto su MenoBot 0.1.1")
+	context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, welcome to MenoBot.")
