72976cd4610e777dfcad7467b9bc3d8bbea1a2d8
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-10-14 10:36:50 +0200

    Improved help message

--- help.py
+++ help.py
@@ -1,3 +1,10 @@
 def help_command(update, context):
-	help_message = "I comandi disponibili sono:\n/start\n/help\n/get_cards"
-	context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)
+	help_message = """I comandi disponibili sono:
+					  <b>help</b> - Show a list of available commands
+					  <b>setup</b> - Start using MenoBot
+					  <b>list_most_wanted_cards</b> - Show most wanted cards on CardMarket
+					  <b>list_my_cards</b> - Show the cards you are following
+					  <b>add_card</b> card - Start following a card
+					  <b>remove_card</b> card_number - Stop following a card
+					  <b>search</b> card - Looks for a card on CardMarket"""
+	context.bot.send_message(chat_id=update.effective_chat.id, text=help_message, parse_mode="HTML")
