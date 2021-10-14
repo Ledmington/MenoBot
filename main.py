680b94d02bc05db9b70dd27de19cfc44a7235c66
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-10-14 09:28:07 +0200

    Cleanup

--- main.py
+++ main.py
@@ -33,26 +33,6 @@ def main() -> None:
 		fallbacks = []
 	)
 
-	'''
-	start_handler = CommandHandler('start', start.start_command)
-	dispatcher.add_handler(start_handler)
-
-	help_handler = CommandHandler('help', help.help_command)
-	dispatcher.add_handler(help_handler)
-
-	most_wanted_list_handler = CommandHandler('list_most_wanted_cards', cards_list.get_most_wanted_cards)
-	dispatcher.add_handler(most_wanted_list_handler)
-
-	my_card_list_handler = CommandHandler('list_my_cards', cards_list.get_my_cards)
-	dispatcher.add_handler(my_card_list_handler)
-
-	add_card_handler = CommandHandler('add_card', cards_list.add_card)
-	dispatcher.add_handler(add_card_handler)
-
-	search_handler = CommandHandler('search', search.search_card)
-	dispatcher.add_handler(search_handler)
-	'''
-
 	dispatcher.add_handler(conv_handler)
 
 	updater.start_polling()
