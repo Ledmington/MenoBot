d279574008cb0b65ad39c8722a2ae8a940aedccf
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-10-17 20:17:01 +0200

    Added Card class

---
+++ card.py
@@ -0,0 +1,31 @@
+import datetime
+
+class Card:
+	# Full name
+	name = ""
+
+	# Full url to reach the card
+	url = ""
+
+	# List of tuples (price, datetime) of last 30 days
+	prices = []
+
+	def __init__(self, card_name, card_url):
+		self.name = card_name.copy()
+		self.url = card_url.copy()
+		self.prices.append((float(0), datetime.datetime.now()))
+
+	def get_name(self):
+		return self.name
+
+	def get_url(self):
+		return self.url
+
+	def get_prices(self):
+		return self.prices
+
+	def get_last_update(self):
+		return prices[-1][1]
+
+	def update_price(self, new_price):
+		self.prirces.append((float(new_price), datetime.datetime.now()))
